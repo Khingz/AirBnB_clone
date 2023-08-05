aaf42abb4f62d62abad314bc094da765cd40f205
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2023-08-05 22:02:33 +0100

    fix reference to instance attributes by prefixing it with self

--- models/base_model.py
+++ models/base_model.py
@@ -11,17 +11,17 @@ class BaseModel:
     """
     def __init__(self):
         """Initialize instance variables"""
-        id = str(uuid.uuid4())
-        created_at = datetime.datetime.now()
-        updated_at = datetime.datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Return string representation of class"""
-        return("Hello world")
+        return ("{}, {}".format(self.updated_at, self.id))
     
     def save(self):
         """Updates the updated_at attribute to current time"""
-        updated_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """Print dictionary representation of class attriutes and method"""
