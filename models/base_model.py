8e3575aa501295fda8f18075cdfe5eb7a27223ec
Khingz <kingsley99m5@gmail.com>
2023-08-06 08:13:42 +0100

    feat: creation of base instance from dict

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,16 @@ class BaseModel:
     """
     A class that defines the basemodel
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize instance variables"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Return string representation of class"""
