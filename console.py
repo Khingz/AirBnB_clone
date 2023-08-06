786c3f25bc186fb56ecd723c8bf755884e855755
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2023-08-06 22:13:11 +0100

    feat: create class instane on command prompt

--- console.py
+++ console.py
@@ -4,12 +4,37 @@ Console for airnb clone console
 """
 
 import cmd
+from models.base_model import BaseModel
 
 class  HBNBCommand(cmd.Cmd):
     """
     class definition for the console
     """
     prompt = '(hbnb) '
+
+    class_mapping = {
+        'BaseModel': BaseModel
+    }
+
+    def error_helper(self, error_type):
+        """Helper function to print error messages using a dictionary"""
+        errors = {
+            "missing_class": "** class name missing **",
+            "invalid_class": "** class doesn't exist **",
+        }
+        print(errors.get(error_type, "Error: Unknown error"))
+
+    def do_create(self, cls):
+        """creates a class instance"""
+        if not cls:
+            self.error_helper("missing_class")
+        elif cls not in self.class_mapping:
+            self.error_helper('invalid_class')
+        else:
+            instance = (self.class_mapping)[cls]()
+            instance.save()
+            print(instance.id)
+
 
     def do_EOF(self, line):
         """
